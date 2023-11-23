0e35979c72bbd5f6ca1f37ffecc6ef1dc7ca039e
OlivierBOROTclassAccount <olivier.borot.poubelle@gmail.com>
2023-11-23 11:52:50 +0100

    first commit

---
+++ src/app/api/login/route.js
@@ -0,0 +1,50 @@
+export async function GET(req, res) {
+  // Make a note we are on
+  // the api. This goes to the console.
+  console.log("in the login api page");
+
+  // get the values
+  // that were sent across to us.
+  const { searchParams } = new URL(req.url);
+  const emailF = searchParams.get("email");
+  const pass = searchParams.get("pass");
+
+  console.log(emailF);
+  console.log(pass);
+
+  // database call goes here
+  const { MongoClient } = require("mongodb");
+  const url = "mongodb://root:example@localhost:27017/";
+  const client = new MongoClient(url);
+  const dbName = "App"; // Nom de la base de données
+
+  try {
+    await client.connect();
+    console.log("Successfully connected to MongoDB server");
+    const db = client.db(dbName);
+    const collection = db.collection("Login"); // Nom de la collection
+    const findResult = await collection
+      .find({ "username": emailF, "pass": pass })
+      .toArray();
+    console.log("Found docs =>", findResult);
+
+    let valid = false;
+    if (findResult.length > 0) {
+      valid = true;
+      console.log("Login valid");
+    } else {
+      console.log("Login invalid");
+    }
+
+
+    return Response.json({ data: "" + valid + "" });
+  } catch (error) {
+    console.error("An error occured :", error);
+  } finally {
+    await client.close();
+  }
+
+  main();
+
+  return Response.json({ data: "valid" });
+}
